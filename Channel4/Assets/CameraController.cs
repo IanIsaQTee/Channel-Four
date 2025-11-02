65fe3482585e42db1fb7839dc2ef603a2950e3e6
Ian <70803354+IanIsaQTee@users.noreply.github.com>
2025-11-02 17:03:22 -0500

    TV Text modified
    size and constraints edited

--- Channel4/Assets/CameraController.cs
+++ Channel4/Assets/CameraController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class CameraManager : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     public Camera CurrCam;
     public CameraNeighbours neighbours;
